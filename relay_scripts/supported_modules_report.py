00e7462dd594f3c1fa28dc20e9e83247eec3dced
Ben Ford <ben.ford@puppetlabs.com>
2022-06-28 16:41:54 -0700

    Port the report to markdown
    The email form will be slightly harder to read, but I plan on shutting
    that off relatively soon anyway.

--- relay_scripts/supported_modules_report.py
+++ relay_scripts/supported_modules_report.py
@@ -35,52 +35,61 @@ for mod in modules:
     except Exception as e:
         print('Could not process module {0}: {1}'.format(mod['slug']))
 
-template = """
+template = """# Module Repository Housekeeping Audit
+
 {%- if tag_module -%}
-The following GitHub repositories appear to be missing the 'module' topic:
+## The following GitHub repositories appear to be missing the 'module' topic:
+
 {%- for item in tag_module %}
-    * https://github.com/puppetlabs/{{ item['name'] }}
+* [{{ item['name'] }}](https://github.com/puppetlabs/{{ item['name'] }})
 {%- endfor %}
 {%- endif %}
 {%- if incomplete %}
 
 
-The following GitHub repositories should have topics clarifying which support tier they fall into:
+## The following GitHub repositories should have topics clarifying which support tier they fall into:
+
 {%- for item in incomplete %}
-    * https://github.com/{{ item }}
+* [{{ item }}](https://github.com/{{ item }})
 {%- endfor %}
 {%- endif %}
 {%- if unmarked %}
 
 
-The following GitHub repositories appear to be missing the support tier README note:
+## The following GitHub repositories appear to be missing the support tier README note:
+
 {%- for item in unmarked %}
-    * https://github.com/{{ item }}
+* [{{ item }}](https://github.com/{{ item }})
 {%- endfor %}
 {%- endif %}
 {%- if badge_supported %}
 
 
-The following Forge modules should be badged as Supported:
+## The following Forge modules should be badged as Supported:
+
 {%- for item in badge_supported %}
-    * https://forge.puppet.com/puppetlabs/{{ item['name'] }}
+* [{{ item['name'] }}](https://forge.puppet.com/puppetlabs/{{ item['name'] }})
 {%- endfor %}
 {%- endif %}
 {%- if badge_unsupported %}
 
 
-The following Forge modules should have the Supported badge removed:
+## The following Forge modules should have the Supported badge removed:
+
 {%- for item in badge_unsupported %}
-    * https://forge.puppet.com/puppetlabs/{{ item['name'] }}
+* [{{ item['name'] }}](https://forge.puppet.com/puppetlabs/{{ item['name'] }})
 {%- endfor %}
 {%- endif %}
 {%- if source_field_problem %}
 
 
-The following Forge modules have a problem with their source field. Either
-the field could not be parsed, or it does not point to a valid public repo:
+## The following Forge modules have a problem with their source field:
+
+Either the field could not be parsed, or it does not point to a valid public repo
+within the org. Often this indicates that the repo has been archived into the Toy Chest.
+
 {%- for item in source_field_problem %}
-    * https://forge.puppet.com/puppetlabs/{{ item['name'] }}
+* [{{ item['name'] }}](https://forge.puppet.com/puppetlabs/{{ item['name'] }})
 {%- endfor %}
 {%- endif %}
 """
